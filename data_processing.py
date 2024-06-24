cc6fce3e6e869686e2ca214966ee55d406c9f54b
Seunghyeok Lee <seunghyeok.tech@gmail.com>
2024-06-25 00:07:32 +0900

    단일 사진 클래스 개수 확인

--- data_processing.py
+++ data_processing.py
@@ -1,4 +1,6 @@
 import os
+from PIL import Image, ImageOps
+import numpy as np
 import zipfile
 
 def extract_zip(zip_file_path, extract_to_path):
@@ -6,3 +8,17 @@ def extract_zip(zip_file_path, extract_to_path):
     with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
         zip_ref.extractall(extract_to_path)
     print(f"{extract_to_path} 경로에 압축을 해제합니다.")
+
+def process_image(label_image_path):
+    """단일 사진 클래스 개수 확인."""
+    # 이미지 로드 및 nupmy 배열로 변환
+    label_image = Image.open(label_image_path)
+    label_image_array = np.array(label_image)
+
+    # 레이블 이미지의 고유값과 분포 시각화
+    unique, counts = np.unique(label_image_array, return_counts=True)
+
+    # 고유값 분포를 프린트
+    print("Unique pixel values in label image:", unique)
+    print("Counts for each value:", counts)
+    return label_image, label_image_array
--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-from data_processing import extract_zip
+from data_processing import extract_zip, process_image
 
 def main():
     # 데이터 압축 해제
     zip_file_path = 'path_to_your_zip_file.zip'
     extract_to_path = 'path_to_extract'
     extract_zip(zip_file_path, extract_to_path)
+
+    label_image, label_image_array = process_image("사진경로/train/targets/guatemala-volcano_00000000_pre_disaster_target.png")
+    
 
 if __name__ == "__main__":
     main()
